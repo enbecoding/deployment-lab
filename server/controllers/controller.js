77fa3764615ee6a7d80d95e1f75b0023413d5d73
Brandon Hoang <brandon.hoang@devmounta.in>
2022-08-16 15:14:22 -0500

    changed everything

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -2,13 +2,13 @@ const path = require("path");
 
 module.exports = {
     getAdHome: (req, res) => {
-        req.sendFile(path.join(__dirname, "../../client/index.html"))
+        res.sendFile(path.join(__dirname, "../../client/index.html"))
     },
     getCSS: (req, res) => {
-        req.sendFile(path.join(__dirname, "../../client/index.css"))
+        res.sendFile(path.join(__dirname, "../../client/index.css"))
     },
     getJS: (req, res) => {
-        req.sendFile(path.join(__dirname, "../../client/main.js"))
+        res.sendFile(path.join(__dirname, "../../client/main.js"))
     }
 
 }
